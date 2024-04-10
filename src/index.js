86e84a52451888dc8e060eb7a7f611cea5e2b754
lei <littlesomerest@gmail.com>
2024-04-10 14:52:31 +0800

    revert changes

--- src/index.js
+++ src/index.js
@@ -141,7 +141,7 @@ function Order({ openHour, closeHour }) {
         We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
         online
       </p>
-      <btn is="btn">Order</btn>
+      <btn className="btn">Order</btn>
     </div>
   );
 }
